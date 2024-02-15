49247a14f2ec951f1b4f7fa9494246e1aa13e281
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-02-15 09:34:18 +0100

    create start page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ textContent, handleClick, style = ""}) {
+export default function Button({ textContent, handleClick}) {
   return (
     <button
-      className={`bg-green-800 text-green-50 hover:bg-green-50 focus:bg-green-50 hover:text-green-800 focus:text-green-800 text-3xl font-semibold tracking-wider py-4 px-8 rounded ${style}`}
+      className="bg-green-800 text-green-50 hover:bg-green-50 focus:bg-green-50 hover:text-green-800 focus:text-green-800 text-3xl font-semibold tracking-wider py-4 px-8 rounded"
       onClick={handleClick}
     >
       {textContent}
--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -13,7 +13,6 @@ export default function StartGame() {
       <Button
         textContent="Start game!"
         handleClick={() => setIsGameOn(true)}
-        // style="mx-auto my-auto"
       />
     </div>
   )
